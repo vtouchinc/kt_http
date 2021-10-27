0a501cb2628ca8eae0dd840f7087e1652f149de4
zic <jhjung@vtouch.io>
2021-10-27 19:57:34 +0900

    .

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
+	console.log( "get " + Date.now() );
   res.send('Hello World!')
 })
 
 app.post('/', (req, res) => {
+	console.log( "post " + Date.now() );
   res.send('Hello World!')
 })
 
