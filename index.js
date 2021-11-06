7e713666a9a2c32c63aa885d668acbbdc78622f5
zic <jhjung@vtouch.io>
2021-11-07 01:03:24 +0900

    computation

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+let lock = false;
+
 app.get('/', (req, res) => {
+
 	console.log( "get " + Date.now()  + req.connection.remoteAddress);
-	setTimeout( ()=>{
+	
+	let xt = 1;
+	for( let i = 1; i < 10000000000 ; i ++){
+		xt = xt * i;
+	}
   res.send('Hello World!')
-	},1000);
 })
 
-app.post('/', (req, res) => {
+//app.post('/', (req, res) => {
 	//console.log( "post " + Date.now()  + req.connection.remoteAddress);
   //res.send('Hello World!')
-})
+//})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
