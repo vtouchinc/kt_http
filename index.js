c8c258c58a6c6a3e3f9186f1375b956a68fc4aed
zic <jhjung@vtouch.io>
2021-11-07 00:34:58 +0900

    slow

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const port = 8080
 
 app.get('/', (req, res) => {
 	console.log( "get " + Date.now()  + req.connection.remoteAddress);
+	setTimeout( ()=>{
   res.send('Hello World!')
+	},1000);
 })
 
 app.post('/', (req, res) => {
-	console.log( "post " + Date.now()  + req.connection.remoteAddress);
-  res.send('Hello World!')
+	//console.log( "post " + Date.now()  + req.connection.remoteAddress);
+  //res.send('Hello World!')
 })
 
 app.listen(port, () => {
